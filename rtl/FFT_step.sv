5fecd07cb3367b8ae63c331062cab949dabd413b
simplyarsh <arshnoorfly@ucla.edu>
2024-04-09 21:09:20 -0700

    Sequential FFT - not working

--- rtl/FFT_step.sv
+++ rtl/FFT_step.sv
@@ -1,100 +1,64 @@
-module FFT_step #(parameter SAMPLES = 4, WIDTH = 32, STAGE = 0)
+module FFT_step #(parameter SAMPLES = 4, WIDTH = 32, TOTAL_STAGES = 4)
 (
+	input clk,
 	input [WIDTH-1:0] sampleInputs [SAMPLES-1:0],
-	output [WIDTH-1:0] display_stream1_1 [SAMPLES-1:0]
+	input [$clog2(SAMPLES)-1:0] stage_number,
+	output reg [WIDTH-1:0] display_stream1_1 [SAMPLES-1:0]
 );
 
-	genvar i;
-	generate
-		for(i=0; i<((SAMPLES >> 1) >> STAGE); i++)
-		begin : OUTER_LOOP
-			genvar j;
-			for(j=0; j<(1<<STAGE); j++)
-			begin : INNER_LOOP
-				logic [(WIDTH-1):0] twiddle_factor_to_use2;
-				logic [STAGE:0] which_factor2;
-				logic [1:0] signOfMultiplication [1:0];
-				
-				
-				
-				//if (STAGE==0) assign which_factor2 = 0;
-				//else			  assign which_factor2 = (i*(1<<STAGE) + j);
-				if (STAGE!=0) assign which_factor2[STAGE-1:0] = (i*(1<<STAGE) + j);
-				assign which_factor2[STAGE] = 0;
-				
-				
-				twiddleFactorCalculator #(2**(STAGE+1), WIDTH)
-				getTwiddleForIteration2
-				(
-					.which_factor(which_factor2),
-					.result(twiddle_factor_to_use2)
-				);
-				
-				ButterflyUnit #(SAMPLES, WIDTH)
-				butterfly2
-				(
-					.input1(sampleInputs[(i*((1<<STAGE)<<1))+j]),
-					.input2(sampleInputs[(i*((1<<STAGE)<<1))+j+(1<<STAGE)]),
-					.twiddle(twiddle_factor_to_use2),
-					.output1(display_stream1_1[(i*((1<<STAGE)<<1))+j]),
-					.output2(display_stream1_1[(i*((1<<STAGE)<<1))+j+(1<<STAGE)]),
-					.signOfMultiplication(signOfMultiplication)
-				);
-			end
-		end
-	endgenerate
-	
-	
-		
-endmodule
+	logic [$clog2(SAMPLES)-1:0] idx = 0;
+	logic [$clog2(SAMPLES)-1:0] jdx = 0;
+	reg jdx_finish = 0;
+	logic [TOTAL_STAGES:0] which_factor2;
+	logic [1:0] signOfMultiplication [1:0];
+	logic [WIDTH-1:0] in1, in2, out1, out2;
+	logic [$clog2(SAMPLES)-1:0] in1_dx, in2_dx, out1_dx, out2_dx;
+	logic [WIDTH-1:0] twiddle_factor_to_use2;
+	logic [WIDTH-1:0] dispOut1, dispOut2;
+	logic [TOTAL_STAGES:0] which_factor2_MASK;
+	assign which_factor2_MASK[TOTAL_STAGES:0] = 1<<<stage_number;
 
-module FFT_step #(parameter SAMPLES = 4, WIDTH = 32)
-(
-	input [WIDTH-1:0] sampleInputs [SAMPLES-1:0],
-	input [$clog2(SAMPLE)-1:0] stage_number;
-	output [WIDTH-1:0] display_stream1_1 [SAMPLES-1:0]
-);
+	assign in1_dx = (idx*((1<<stage_number)<<1))+jdx;
+	assign in2_dx = (idx*((1<<stage_number)<<1))+jdx+(1<<stage_number);
+	assign out1_dx = (idx*((1<<stage_number)<<1))+jdx;
+	assign out2_dx = (idx*((1<<stage_number)<<1))+jdx+(1<<stage_number);
 
-	genvar i;
-	generate
-		for(i=0; i<((SAMPLES >> 1) >> STAGE); i++)
-		begin : OUTER_LOOP
-			genvar j;
-			for(j=0; j<(1<<STAGE); j++)
-			begin : INNER_LOOP
-				logic [(WIDTH-1):0] twiddle_factor_to_use2;
-				logic [STAGE:0] which_factor2;
-				logic [1:0] signOfMultiplication [1:0];
-				
-				
-				
-				//if (STAGE==0) assign which_factor2 = 0;
-				//else			  assign which_factor2 = (i*(1<<STAGE) + j);
-				if (STAGE!=0) assign which_factor2[STAGE-1:0] = (i*(1<<STAGE) + j);
-				assign which_factor2[STAGE] = 0;
-				
-				
-				twiddleFactorCalculator #(2**(STAGE+1), WIDTH)
-				getTwiddleForIteration2
-				(
-					.which_factor(which_factor2),
-					.result(twiddle_factor_to_use2)
-				);
-				
-				ButterflyUnit #(SAMPLES, WIDTH)
-				butterfly2
-				(
-					.input1(sampleInputs[(i*((1<<STAGE)<<1))+j]),
-					.input2(sampleInputs[(i*((1<<STAGE)<<1))+j+(1<<STAGE)]),
-					.twiddle(twiddle_factor_to_use2),
-					.output1(display_stream1_1[(i*((1<<STAGE)<<1))+j]),
-					.output2(display_stream1_1[(i*((1<<STAGE)<<1))+j+(1<<STAGE)]),
-					.signOfMultiplication(signOfMultiplication)
-				);
-			end
+	twiddleFactorCalculator #(2**(TOTAL_STAGES+1), WIDTH)
+	getTwiddleForIteration2
+	(
+		.which_factor(which_factor2),
+		.result(twiddle_factor_to_use2)
+	);
+
+	ButterflyUnit #(SAMPLES, WIDTH)
+	butterfly2
+	(
+		.input1(sampleInputs[in1_dx]),
+		.input2(sampleInputs[in2_dx]),
+		.twiddle(twiddle_factor_to_use2),
+		.output1(dispOut1),
+		.output2(dispOut2),
+		.signOfMultiplication(signOfMultiplication)
+	);
+
+	
+	always @(posedge clk) begin
+		display_stream1_1[out1_dx] = dispOut1;
+		display_stream1_1[out2_dx] = dispOut2;
+		if (stage_number!=0)
+			which_factor2[TOTAL_STAGES:0] = (idx*(1<<stage_number) + jdx)&which_factor2_MASK;
+		which_factor2[stage_number] = 0;
+
+		if (idx < ((SAMPLES >> 1) >> stage_number) && jdx_finish)
+			idx <= idx + 1;
+		if (jdx < (1<<stage_number)) begin
+			jdx <= jdx + 1;
+			jdx_finish <= 0;
+		end else begin
+			jdx <= 0;
+			jdx_finish <= 1;
 		end
-	endgenerate
-	
-	
+	end
+
 		
 endmodule
--- tb/FFT_step1_tb.sv
+++ tb/FFT_step1_tb.sv
@@ -1,3 +1,4 @@
+`timescale 1ps/1ps
 module FFT_step1_tb #(parameter SAMPLES = 4, WIDTH = 32, STAGE = 0)
 (
 	output [WIDTH-1:0] display_stream1_1 [SAMPLES-1:0]
@@ -5,6 +6,7 @@ module FFT_step1_tb #(parameter SAMPLES = 4, WIDTH = 32, STAGE = 0)
 
 
 	logic [WIDTH-1:0] sampleInputs [SAMPLES-1:0];
+	logic clk = 0;
 	/*
 	assign sampleInputs[0] = 'h12c;
 	assign sampleInputs[1] = 'hff9c;
@@ -20,8 +22,18 @@ module FFT_step1_tb #(parameter SAMPLES = 4, WIDTH = 32, STAGE = 0)
 	FFT_step #(SAMPLES, WIDTH, STAGE)
 	tester
 	(
+		.clk(clk),
 		.sampleInputs(sampleInputs),
+		.stage_number(0),
 		.display_stream1_1(display_stream1_1)
 	);
+
+	initial begin
+		$dumpfile("dump.vcd");
+		$dumpvars(0);
+		forever begin
+			#1 clk = ~clk;
+		end
+	end
 		
 endmodule
