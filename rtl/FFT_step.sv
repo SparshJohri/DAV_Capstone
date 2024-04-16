466687c5065b5ca3cc3071f61c5269ef6495d48e
SparshJohri2023 <sparshjohri2004@gmail.com>
2024-04-16 13:26:00 -0700

    SEQUENTIALIZED FFT WORKINGgit status!

--- rtl/FFT_step.sv
+++ rtl/FFT_step.sv
@@ -1,9 +1,10 @@
 module FFT_step #(parameter SAMPLES = 4, WIDTH = 32, TOTAL_STAGES = 4)
 (
-	input clk,
+	input clk, start,
 	input [WIDTH-1:0] sampleInputs [SAMPLES-1:0],
 	input [$clog2(SAMPLES)-1:0] stage_number,
-	output reg [WIDTH-1:0] display_stream1_1 [SAMPLES-1:0]
+	output reg [WIDTH-1:0] display_stream1_1 [SAMPLES-1:0],
+	output reg done
 );
 
 	logic [$clog2(SAMPLES)-1:0] idx = 0;
@@ -11,21 +12,22 @@ module FFT_step #(parameter SAMPLES = 4, WIDTH = 32, TOTAL_STAGES = 4)
 	reg jdx_finish = 0;
 	logic [TOTAL_STAGES:0] which_factor2;
 	logic [1:0] signOfMultiplication [1:0];
-	logic [WIDTH-1:0] in1, in2, out1, out2;
+	// logic [WIDTH-1:0] in1, in2, out1, out2;
 	logic [$clog2(SAMPLES)-1:0] in1_dx, in2_dx, out1_dx, out2_dx;
 	logic [WIDTH-1:0] twiddle_factor_to_use2;
 	logic [WIDTH-1:0] dispOut1, dispOut2;
 	logic [TOTAL_STAGES:0] which_factor2_MASK;
-	assign which_factor2_MASK[TOTAL_STAGES:0] = 1<<<stage_number;
+	assign which_factor2_MASK[TOTAL_STAGES:0] = (1<<(stage_number))-1;
 
 	assign in1_dx = (idx*((1<<stage_number)<<1))+jdx;
 	assign in2_dx = (idx*((1<<stage_number)<<1))+jdx+(1<<stage_number);
 	assign out1_dx = (idx*((1<<stage_number)<<1))+jdx;
 	assign out2_dx = (idx*((1<<stage_number)<<1))+jdx+(1<<stage_number);
 
-	twiddleFactorCalculator #(2**(TOTAL_STAGES+1), WIDTH)
+	twiddleFactorCalculator #(TOTAL_STAGES, WIDTH)
 	getTwiddleForIteration2
 	(
+		.SAMPLES(1<<(stage_number+1)),
 		.which_factor(which_factor2),
 		.result(twiddle_factor_to_use2)
 	);
@@ -41,22 +43,62 @@ module FFT_step #(parameter SAMPLES = 4, WIDTH = 32, TOTAL_STAGES = 4)
 		.signOfMultiplication(signOfMultiplication)
 	);
 
+	always_comb begin
+		if (start) begin
+
+			if (stage_number != 0) begin
+				which_factor2 = (idx*(1<<stage_number) + jdx)&which_factor2_MASK;
+			end else begin
+				which_factor2 = 0;
+			end
+
+		end else begin
+			which_factor2 = 0;
+		end
+
+	end
 	
 	always @(posedge clk) begin
-		display_stream1_1[out1_dx] = dispOut1;
-		display_stream1_1[out2_dx] = dispOut2;
-		if (stage_number!=0)
-			which_factor2[TOTAL_STAGES:0] = (idx*(1<<stage_number) + jdx)&which_factor2_MASK;
-		which_factor2[stage_number] = 0;
+		if (start) begin
+			display_stream1_1[out1_dx] = dispOut1;
+			display_stream1_1[out2_dx] = dispOut2;
 
-		if (idx < ((SAMPLES >> 1) >> stage_number) && jdx_finish)
-			idx <= idx + 1;
-		if (jdx < (1<<stage_number)) begin
-			jdx <= jdx + 1;
-			jdx_finish <= 0;
+			if (idx == ((SAMPLES >> 1) >> stage_number) -1 )
+			begin
+				if (jdx == (1<<stage_number) - 1)
+				begin
+					idx <= 0;
+					jdx <= 0;
+					done <= 1;
+				end
+
+				else
+				begin
+					jdx <= jdx+1;
+					done <= 0;
+				end
+			end
+
+			else
+			begin
+				if (jdx == (1<<stage_number) - 1)
+				begin
+					jdx <= 0;
+					idx <= idx+1;
+					done <= 0;
+				end
+
+				else
+				begin
+					jdx <= jdx+1;
+					done <= 0;
+				end
+			end
+
 		end else begin
+			idx <= 0;
 			jdx <= 0;
-			jdx_finish <= 1;
+			done <= 0;
 		end
 	end
 
--- rtl/N_point_fft_seq.sv
+++ rtl/N_point_fft_seq.sv
@@ -1,7 +1,6 @@
 // below is the sequential version without a 3d array
 module N_point_fft_seq #(
-	parameter WIDTH = 0,
-	parameter SAMPLES = 0
+	parameter WIDTH = 32, SAMPLES = 16
 )(
 	input clk, input rst,
 	input [WIDTH-1:0] sampleInputs [SAMPLES-1:0],
@@ -9,11 +8,21 @@ module N_point_fft_seq #(
 	output [WIDTH-1:0] outputs [SAMPLES-1:0]
 );
 
-
 	logic [$clog2(SAMPLES)-1:0] new_indices [SAMPLES-1:0];
 	reg [WIDTH-1:0] fft_in [SAMPLES-1:0];
+	reg [WIDTH-1:0] fft_in_d [SAMPLES-1:0];
 	reg [WIDTH-1:0] fft_out [SAMPLES-1:0];
+	logic cycle_done;
 	wire [WIDTH-1:0] prepped_out [SAMPLES-1:0];
+	logic start_fft_step;
+
+	// state management
+	localparam IDLE = 0;
+	localparam RUN = 1;
+	localparam FINISH = 2;
+
+	logic [1:0] state, next_state;
+	logic [$clog2(SAMPLES)-1:0] cycle_cntr, cycle_cntr_d;
 
 	scrambleForFFT_tb #(SAMPLES, $clog2(SAMPLES)) get_new_indices
 	(
@@ -29,34 +38,33 @@ module N_point_fft_seq #(
 
 	FFT_step #(SAMPLES, WIDTH, $clog2(SAMPLES)) takeStep
 	(
+		.start(start_fft_step),
 		.clk(clk),
 		.sampleInputs(fft_in), 
 		.stage_number(cycle_cntr),
-		.display_stream1_1(fft_out) 
+		.display_stream1_1(fft_out),
+		.done(cycle_done)
 	);
 
-	// state management
-	localparam IDLE = 0;
-	localparam RUN = 0;
-	localparam FINISH = 0;
-
-	logic [1:0] state, next_state;
-	logic [$clog2(SAMPLES)-1:0] cycle_cntr, cycle_cntr_d;
-
 	assign out_valid = state == FINISH ? 1'b1 : 1'b0; 
-	assign outputs = fft_out;
+	assign outputs = fft_in;
 
 	always @(*) begin
-		cycle_cntr_d  = 0;
+		cycle_cntr_d  = cycle_cntr;
+		fft_in_d = fft_in;
+		start_fft_step = 0;
 		case (state)
 			IDLE: begin
-                for (int i =0; i < SAMPLES; i++) begin
-				    fft_in[i] = prepped_out[i];
-                end
+                fft_in_d = prepped_out;
 				next_state = RUN;
 			end
 			RUN: begin
-				cycle_cntr_d = cycle_cntr + 1;
+				if (cycle_done) begin
+					start_fft_step = 0;
+					cycle_cntr_d = cycle_cntr + 1;
+					fft_in_d = fft_out;
+				end else
+					start_fft_step = 1;
 				if (cycle_cntr == $clog2(SAMPLES)) begin
 					next_state = FINISH;
 				end
@@ -69,15 +77,15 @@ module N_point_fft_seq #(
 		endcase
 	end
 
-	always @(clk) begin
+	always @(posedge clk) begin
 		if (rst) begin
 			state <= IDLE;
+			cycle_cntr = 0;
+			// fft_in <= 0;
 		end else begin
 			state <= next_state;
 			cycle_cntr <= cycle_cntr_d; 
-		end
-		if (state == RUN) begin
-			fft_in <= fft_out;
+			fft_in <= fft_in_d;
 		end
 	end
 
